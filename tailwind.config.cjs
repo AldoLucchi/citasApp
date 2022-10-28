0553e04126f81f28cc9cc0184d87dacce62ac72b
Aldo Lucchi <pokeraldolucchi@hotmail.com>
2022-10-28 03:43:50 -0300

    Primer Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["index.html", "./src/**/*.jsx" ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {}
+  },
+
+  plugins: [],
+};
